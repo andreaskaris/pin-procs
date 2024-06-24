367ca93f0b848dfa024602e2efbce4162144c474
Andreas Karis <ak.karis@gmail.com>
2024-06-24 22:28:51 +0200

    next
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- Makefile
+++ Makefile
@@ -3,11 +3,14 @@ IMAGE ?= quay.io/akaris/pin-procs
 build:
 	CGO_ENABLED=0 GOOS=linux go build -o _output/pin-procs .
 
+clean:
+	rm -f _output/*
+
 container-build:
 	podman build -t $(IMAGE)  .
 
 container-push:
 	podman push $(IMAGE)
 
-clean:
-	rm -f _output/*
+deploy:
+	kubectl apply -f daemonset.yaml
--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path"
 	"regexp"
+	"strconv"
+	"time"
 
 	"github.com/vishvananda/netlink"
 	"golang.org/x/sys/unix"
@@ -16,19 +19,23 @@ import (
 )
 
 const (
-	statusFile                      = "/proc/%d/status"
 	pinModeNumericRegularExpression = `^[0-9]+(-[0-9]+)?(,[0-9]+(-[0-9]+)?)*$`
 	pinModeRegularExpression        = `^first$|^last$|` + pinModeNumericRegularExpression
+	singleCPURegularExpression      = `^[0-9]+$`
 	firstCPURegularExpression       = `^([0-9]+)[^0-9]`
-	lastCPURegularExpression        = `[^0-9]([0-9]+)$*`
+	lastCPURegularExpression        = `[^0-9]([0-9]+)$`
 )
 
 var (
+	procDirectory       = "/proc/"
+	statusFile          = "status"
 	procNameFilter      = flag.String("proc-name-filter", "", "filter for process names to be pinned")
 	pinMode             = flag.String("pin-mode", "", "instruction for vCPU to pin to (accepted values: 'first', 'last', [0-9]+,)")
 	discoveryMode       = flag.Bool("discovery-mode", false, "discovery mode will print all discovered processes that match proc-name-filter")
+	tickSeconds         = flag.Int("tick-seconds", 0, "additionally run logic time based every <tick> seconds (default 0 = never)")
 	pinModeRegex        = regexp.MustCompile(pinModeRegularExpression)
 	pinModeNumericRegex = regexp.MustCompile(pinModeNumericRegularExpression)
+	singleCPURegex      = regexp.MustCompile(singleCPURegularExpression)
 	firstCPURegex       = regexp.MustCompile(firstCPURegularExpression)
 	lastCPURegex        = regexp.MustCompile(lastCPURegularExpression)
 )
@@ -39,7 +46,7 @@ func getProcessAttributes(pid uint32) ([]byte, []byte, error) {
 	var procName []byte
 	var procCPUsAllowedList []byte
 
-	f, err := os.Open(fmt.Sprintf(statusFile, pid))
+	f, err := os.Open(getStatusFile(pid))
 	if err != nil {
 		return nil, nil, err
 	}
@@ -63,14 +70,21 @@ func getProcessAttributes(pid uint32) ([]byte, []byte, error) {
 	return nil, nil, fmt.Errorf("no file name found for pid %d", pid)
 }
 
+func getStatusFile(pid uint32) string {
+	return path.Join(procDirectory, fmt.Sprintf("%d", pid), statusFile)
+}
+
 // pinProcess takes the process ID, the current Cpus_allowed_list of the process, as well as the user provided
 // pinMode. The pinMode can be first (first CPU of current process' Cpus_allowed_list), last (last CPU of current
 // process' Cpus_allowed_list) or an explicit new Cpus_allowed_list for the process. An error indicates a parsin issue,
 // or that the CPU affinity could not be set for the process.
 func pinProcess(pid uint32, currentProcCPUsAllowedList []byte, pinMode string) error {
-	newProcCPUsAllowedList, err := getPinSet(currentProcCPUsAllowedList, pinMode)
+	newProcCPUsAllowedList, needsApply, err := getPinSet(currentProcCPUsAllowedList, pinMode)
 	if err != nil {
 		return err
+	}
+	if !needsApply {
+		return nil
 	}
 
 	var cpuMask unix.CPUSet
@@ -89,28 +103,89 @@ func pinProcess(pid uint32, currentProcCPUsAllowedList []byte, pinMode string) e
 // getPinSet takes the currentProcCPUsAllowedList and the new pinMode. The pinMode can be first (first CPU of current
 // process' Cpus_allowed_list), last (last CPU of current process' Cpus_allowed_list) or an explicit new
 // Cpus_allowed_list for the process. An error indicates that an invalid pinMode was provides, or that
-// currentProcCPUsAllowedList could not be parsed.
-func getPinSet(currentProcCPUsAllowedList []byte, pinMode string) (string, error) {
+// currentProcCPUsAllowedList could not be parsed. The boolean return value indicates if the change must be applied (true),
+// or if nothing changed and the change need not be applied (false).
+func getPinSet(currentProcCPUsAllowedList []byte, pinMode string) (string, bool, error) {
 	if pinModeNumericRegex.MatchString(pinMode) {
-		return pinMode, nil
+		return pinMode, true, nil
 	}
+
+	if singleCPURegex.Match(currentProcCPUsAllowedList) {
+		return string(currentProcCPUsAllowedList), false, nil
+	}
+
 	if pinMode == "first" {
 		sMatches := firstCPURegex.FindSubmatch(currentProcCPUsAllowedList)
 		if len(sMatches) != 2 {
-			return "", fmt.Errorf("pinMode 'first' could not find a valid match in currentProcCPUsAllowedList %q",
+			return "", false, fmt.Errorf("pinMode 'first' could not find a valid match in currentProcCPUsAllowedList %q",
 				currentProcCPUsAllowedList)
 		}
-		return string(sMatches[1]), nil
+		return string(sMatches[1]), true, nil
 	}
 	if pinMode == "last" {
 		sMatches := lastCPURegex.FindSubmatch(currentProcCPUsAllowedList)
 		if len(sMatches) != 2 {
-			return "", fmt.Errorf("pinMode 'first' could not find a valid match in currentProcCPUsAllowedList %q",
+			return "", false, fmt.Errorf("pinMode 'first' could not find a valid match in currentProcCPUsAllowedList %q",
 				currentProcCPUsAllowedList)
 		}
-		return string(sMatches[1]), nil
+		return string(sMatches[1]), true, nil
 	}
-	return "", fmt.Errorf("getPinSet was provided with invalid pinMode: %q", pinMode) // Should never happen.
+	return "", false, fmt.Errorf("getPinSet was provided with invalid pinMode: %q", pinMode) // Should never happen.
+}
+
+// executeForPID runs the logic for a single process ID. Moved into separate function so that we can run it for
+// netlink triggered and ticker triggered events.
+func executeForPID(pid uint32, re *regexp.Regexp, discoveryMode *bool, pinMode *string) {
+	// log.Printf("debug: Looking at process with pid %d", pid)
+	// Get process attributes. If we can't, skip (directory for this process might be missing [process killed?]).
+	procName, procCPUsAllowedList, err := getProcessAttributes(pid)
+	if err != nil {
+		// log.Printf("debug: Skipped, could not get attributes of process with pid %d ", pid)
+		return
+	}
+	// If the process name does not match the user provided filter, skip.
+	if !re.Match(procName) {
+		// log.Printf("debug: Skipped, process with pid %d (%q) did not match regex %q", pid, procName, *procNameFilter)
+		return
+	}
+	// If this is discovery mode, only print the process attributes, but do not pin.
+	if *discoveryMode {
+		log.Printf("PID: %d, Name: %s, cpus_allowed_list: %s\n", pid, procName, procCPUsAllowedList)
+		return
+	}
+	// If this is not discovery mode, pin the process.
+	if err := pinProcess(pid, procCPUsAllowedList, *pinMode); err != nil {
+		log.Printf("Warning: Could not pin process; PID: %d, Name: %s, PinMode: %s, err: %q\n",
+			pid, procName, *pinMode, err)
+	}
+}
+
+// tickMode runs an (optional) ticker every tickSeconds.
+func tickMode(ctx context.Context, re *regexp.Regexp, discoveryMode *bool, pinMode *string, tickSeconds *int) {
+	ticker := time.NewTicker(time.Second * time.Duration(*tickSeconds))
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			log.Printf("Tick - looking for processes at regular interval (every %d seconds)", *tickSeconds)
+			entries, err := os.ReadDir(procDirectory)
+			if err != nil {
+				log.Fatalf("Could not read from proc directory %q, err: %q", procDirectory, err)
+			}
+			var pid int
+			for _, entry := range entries {
+				if entry.IsDir() {
+					if pid, err = strconv.Atoi(entry.Name()); err == nil {
+						executeForPID(uint32(pid), re, discoveryMode, pinMode)
+					}
+				}
+			}
+		case <-ctx.Done():
+			log.Print("Ticker stopped")
+			return
+		}
+	}
 }
 
 func main() {
@@ -133,36 +208,31 @@ func main() {
 	// Can be empty or a valid regexp to find processes by name.
 	re := regexp.MustCompile(*procNameFilter)
 
+	// Check if /host/proc exists, in that case we'll be looking there for process information (important for containerization).
+	if _, err := os.Stat("/host/proc"); err == nil {
+		procDirectory = path.Join("/host", procDirectory)
+	}
+
+	log.Printf("Started with parameters: discovery-mode: %t, pin-mode: %q, proc-name-filter: %q, status-file: %s/%%d/%s",
+		*discoveryMode, *pinMode, *procNameFilter, procDirectory, statusFile)
+
 	// netlink.ProcEventMonitor uses netlink to retrieve updates about processes. Must be run with sufficient privileges.
+	ctx := context.TODO()
 	procChan := make(chan netlink.ProcEvent)
 	errorChan := make(chan error)
 	if err := netlink.ProcEventMonitor(procChan, context.TODO().Done(), errorChan); err != nil {
 		log.Fatalf("Got an error from netlink.ProcEventMonitor during initialization, err: %q", err)
 	}
+	// Add a ticker to regularly scan all processes. The ProcEventMonitor does not work for vhost processes,
+	// unfortunately. It also can't hurt to have some extra logic that will (re)apply the settings every x seconds.
+	if *tickSeconds > 0 {
+		go tickMode(ctx, re, discoveryMode, pinMode, tickSeconds)
+	}
 	for {
 		select {
 		case p := <-procChan:
 			pid := p.Msg.Pid()
-
-			// Get process attributes. If we can't, skip (directory for this process might be missing [process killed?]).
-			procName, procCPUsAllowedList, err := getProcessAttributes(pid)
-			if err != nil {
-				continue
-			}
-			// If the process name does not match the user provided filter, skip.
-			if !re.Match(procName) {
-				continue
-			}
-			// If this is discovery mode, only print the process attributes, but to not pin.
-			if *discoveryMode {
-				log.Printf("PID: %d, Name: %s, cpus_allowed_list: %s\n", pid, procName, procCPUsAllowedList)
-				continue
-			}
-			// If this is not discovery mode, pin the process.
-			if err := pinProcess(pid, procCPUsAllowedList, *pinMode); err != nil {
-				log.Printf("Warning: Could not pin process; PID: %d, Name: %s, PinMode: %s, err: %q\n",
-					pid, procName, *pinMode, err)
-			}
+			executeForPID(pid, re, discoveryMode, pinMode)
 		case err := <-errorChan:
 			log.Fatalf("Got an error from netlink.ProcEventMonitor during select, err: %q", err)
 		}
